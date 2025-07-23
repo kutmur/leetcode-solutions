156000e3f16c61effe4a5f2a2e5ae0040f61fe6b
Halil <halilkutmur@gmail.com>
2025-07-23 14:39:05 +0300

    Solution Added

--- main.cpp
+++ main.cpp
@@ -236,5 +236,6 @@ using namespace std;
 #include "S_141.h"
 #include "S_21.h"
 #include "S_2095.h"
+#include "S_328.h"
 
 #pragma endregion
---
+++ medium/medium/S_328.h
@@ -0,0 +1,12 @@
+#ifndef S_328_H
+#define S_328_H
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#endif // S_328_H
